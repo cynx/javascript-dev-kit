b203397a78874d7d3323eb2063b0174b5375e923
echosteg <email@echosteg.com>
2016-12-22 12:21:00 +0530

    eslint integration

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import numeral from 'numeral';
 import './index.css';
 
 const courseValue = numeral(1000).format('$0,0.00');
-console.log(`the value of this course is ${courseValue}, alright!`);
+console.log(`the value of this course is ${courseValue}, alright!`); //eslint-disable-line no-console
 
